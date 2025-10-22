60dae0cbb9f5702b25bbf63264f9d7384d2e465a
Maze_joni <132272609+mazijoni@users.noreply.github.com>
2025-10-22 13:26:08 +0200

    refactor contact form: remove action URL, update button class, and enhance styling

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('contactForm');
+
+    form.addEventListener('submit', async (e) => {
+    e.preventDefault(); // Prevent default navigation
+
+    const formData = new FormData(form);
+
+    try {
+        const response = await fetch('https://jonatan67.app.n8n.cloud/webhook-test/email-check', {
+        method: 'POST',
+        body: formData
+        });
+
+        if (response.ok) {
+        alert('Message sent successfully!');
+        form.reset();
+        } else {
+        alert('Failed to send message.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred.');
+    }
+    });
